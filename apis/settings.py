47936a999a8e0d6cd006bafe587931523b068a3b
shhwetajha <shhwetajha9354@gmail.com>
2024-03-28 18:23:25 +0530

    modified staticfiles

--- apis/settings.py
+++ apis/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT= BASE_DIR/'staticfiles'
+STATIC_ROOT= BASE_DIR/'static'
 STATICFILES_DIRS=['fapp/static']
 # Python Anywhere deployment settings
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
