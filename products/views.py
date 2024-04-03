5792986dcec8763574964588f33a6759042e1fa5
shhwetajha <shhwetajha9354@gmail.com>
2024-04-03 19:27:42 +0530

    changes in review func

--- products/views.py
+++ products/views.py
@@ -62,16 +62,6 @@ class reviewratingregister(APIView):
     renderer_classes=[UserRenderer]
     permission_classes=[IsAuthenticated]
 
-    def post(self,request,single_productid):
-            product=Products.objects.get(id=single_productid)
-            serializer=reviewratingserialzer(data=request.data,context={'user':request.user,'product':product})
-            if serializer.is_valid():
-                Reviewrating=reviewrating.objects.get(user=request.user)
-                Reviewrating.ip=request.META.get('REMOTE_ADDR')
-                Reviewrating.save()       
-                return Response({'data':'review stored successfully!'},status=status.HTTP_200_OK)
-            else:
-                return Response({'error':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
     try:
         def patch(self,request,single_productid):
             Reviewrating=reviewrating.objects.get(user=request.user,product_id=single_productid)
